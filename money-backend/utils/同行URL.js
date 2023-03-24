76221a94c4a198018552550e3a01fd86f47ab5da
yang <yang@yangdeMacBook-Pro.local>
2023-03-24 18:04:08 +0800

    USERPOSTTEXT

--- money-backend/utils/同行URL.js
+++ money-backend/utils/同行URL.js
@@ -2,55 +2,55 @@ module.exports = [
   {
     value:
       'https://www.douyin.com/user/MS4wLjABAAAASRw_LTCR0Xw_tKDero0DUwp2xePByY0n5-UjiHuFCHrwrfYOwTJhBA3sAfCB9ISM',
-    label: '小羊公主',
+    label: '同行/小羊公主',
     desc: '谢谢你的关注',
   },
   {
     value:
       'https://www.douyin.com/user/MS4wLjABAAAAF4P0f-UviR_MX-AFQEInMDL1nNI6wyvydoauqoVSUqCp30p-9cv06ga6ZZwkEiUw',
-    label: '达达不溜',
+    label: '同行/达达不溜',
     desc: '无所谓 我会出手 同款都在👇...',
   },
   {
     value:
       'https://www.douyin.com/user/MS4wLjABAAAAGgFc6LizdSYwzcClfTiaegpDn9eqqFS5mq4672wcGuk',
-    label: '莓川库茶',
+    label: '同行/莓川库茶',
     desc: '视频同款再',
   },
   {
     value:
       'https://www.douyin.com/user/MS4wLjABAAAAno3hVuTejhvlTm86Ck5OU0pxjbDUOFnSo7sL--pAO98gkn1wfBT3DHXcZOyWO_0G',
-    label: '_与甜甜',
+    label: '同行/_与甜甜',
     desc: '生活总要有点盼头 不然漫漫长路真无趣. ：x13003134...',
   },
   {
     value:
       'https://www.douyin.com/user/MS4wLjABAAAAyg6BV_9IiXSCwmOn0O8_TgchEaHy_GWXx71aG0CVBqs',
-    label: '章鱼丸吱',
+    label: '同行/章鱼丸吱',
     desc: '分享小生活 商ZyJz806 小登西都在 盗视频必究...',
   },
   {
     value:
       'https://www.douyin.com/user/MS4wLjABAAAA3aYne4jZQgdO0Z7Kj3zGwL6yhvg7jw7srAKbSPKmHRWlEMYO7JsNbOXMXDpprKat?vid=7209146419628870968',
-    label: '鲨气疼疼',
+    label: '同行/鲨气疼疼',
     desc: '分享好玩的小东西bot 你想要的同款在 找不到喊我...',
   },
   {
     value:
       'https://www.douyin.com/user/MS4wLjABAAAAWHKVcRzaBT0cB6Imk39ABPqsZTIHamZkcFDmrhRFt6s',
-    label: '乐林好物',
+    label: '同行/乐林好物',
     desc: '我是乐林， 爱生活，爱分享 居家好物，生活百货品在 点左下角小黄车可以...',
   },
   {
     value:
       'https://www.douyin.com/user/MS4wLjABAAAAc1psH2X0JDFrH3sBzn7a3Z60FzNbkgyPs1VOrmTukDddwuD_Cb5u5Pl7i0zaLa1v',
-    label: '乔乔好物',
+    label: '同行/乔乔好物',
     desc: '我是乔乔，酷爱尝试各种新奇特小家居！超爱买买买～每天都会直播！给粉丝们推荐我亲测...',
   },
   {
     value:
       'https://www.douyin.com/user/MS4wLjABAAAAZ3MxDkfGyCUSX0eytHGR0shi8aou4Rd0kBIbDvNZVOY',
-    label: '一毛栗子',
+    label: '同行/一毛栗子',
     desc: '辉煌哥@栗子的饲养员 栗子的好物分享@栗子五分熟 这是我们在一起的第九个年头啦...',
   },
   // {
