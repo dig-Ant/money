28591dc29508cee3c5f2cfbfd85dacf5461881ba
yang <yang@yangdeMacBook-Pro.local>
2023-03-24 09:42:39 +0800

    grtLink优化

--- money-backend/core/puppFeature/feature_grtLink.js
+++ money-backend/core/puppFeature/feature_grtLink.js
@@ -5,6 +5,7 @@ let linkList = require('../../downloadFiles/link');
 
 const feature_grtLink = async function (params = {}) {
   try {
+    linkList = linkList.map((e) => 'http' + e.split('http')[1]);
     console.log(linkList);
     const { browser, page } = await this.createBrowser({
       launchKey: 'feature_grtLink',
@@ -14,40 +15,30 @@ const feature_grtLink = async function (params = {}) {
 
     await page.setViewport({ width: 1080, height: 800 });
     let resLinkList = [];
-    await limitExec(
-      async (link) => {
-        try {
-          const videoPage = await browser.newPage();
-          await videoPage.goto(link, TIME_OUT);
-          await videoPage.waitForSelector('.Nu66P_ba', TIME_OUT);
+    for (let i = 0, link = linkList[i]; i < linkList.length; i++) {
+      await page.goto(link, TIME_OUT);
+      await page.waitForSelector('.Nu66P_ba', TIME_OUT);
 
-          const url = await videoPage.url();
-          // const title = await videoPage.title();
-          console.log('link,url: ', link, url);
-          // await videoPage.waitForSelector('.N4QS6RJT', TIME_OUT);
-          const { username, desc } = await videoPage.evaluate(async () => {
-            try {
-              const [username, desc] = [
-                ...document.querySelectorAll('.Nu66P_ba'),
-              ].map((e) => (e || {}).innerText || '');
-              //   const desc = document.querySelector('meta[name=description]');
-              return { username, desc };
-            } catch (error) {
-              console.log(error);
-              debugger;
-            }
-          });
-          console.log(username);
-          resLinkList.push({ value: url, label: username, desc });
-          videoPage.close();
+      const url = await page.url();
+      // const title = await page.title();
+      console.log('link,url: ', link, url);
+      // await page.waitForSelector('.N4QS6RJT', TIME_OUT);
+      const { username, desc } = await page.evaluate(async () => {
+        try {
+          const [username, desc] = [
+            ...document.querySelectorAll('.Nu66P_ba'),
+          ].map((e) => (e || {}).innerText || '');
+          //   const desc = document.querySelector('meta[name=description]');
+          return { username, desc };
         } catch (error) {
-          videoPage.close();
-          console.log('error: ---', error);
-          return { code: -1, errorMsg: error };
+          console.log(error);
+          debugger;
         }
-      },
-      linkList.map((e) => 'http' + e.split('http')[1]),
-    );
+      });
+      console.log(username);
+      resLinkList.push({ value: url, label: username, desc });
+    }
+    page.close();
     console.log(resLinkList);
     return { list: resLinkList };
   } catch (error) {
